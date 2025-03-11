b4beb8ba61fe7ca406e2314a6775a5c2ef5ef8bd
Murilo Trevejo <murilotrevejo09@gmail.com>
2025-03-11 17:43:49 -0300

    fix tests

--- src/main/java/com/example/automatodoc/CPFAutomaton.java
+++ src/main/java/com/example/automatodoc/CPFAutomaton.java
@@ -1,8 +1,10 @@
 package com.example.automatodoc;
 
+import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class CPFAutomaton {
 
   // Enum para representar os estados do autômato
--- src/test/java/com/example/automatodoc/AutomatodocApplicationTests.java
+++ src/test/java/com/example/automatodoc/AutomatodocApplicationTests.java
@@ -1,13 +1,83 @@
 package com.example.automatodoc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AutomatodocApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private CPFAutomaton cpfAutomaton;
 
+  @Test
+  void contextLoads() {
+  }
+
+  @Test
+  void testValidCPFWithDotsAndDash() {
+    assertThat(cpfAutomaton.validateCPF("123.123.123-12")).isTrue();
+  }
+
+  @Test
+  void testValidCPFWithoutDotsAndDash() {
+    assertThat(cpfAutomaton.validateCPF("12312312312")).isTrue();
+  }
+
+  @Test
+  void testInvalidCPFWithIncorrectFormat1() {
+    assertThat(cpfAutomaton.validateCPF("123.12312312")).isFalse();
+  }
+
+  @Test
+  void testInvalidCPFWithIncorrectFormat2() {
+    assertThat(cpfAutomaton.validateCPF("1231.231.23-12")).isFalse();
+  }
+
+  @Test
+  void testInvalidCPFWithIncorrectFormat3() {
+    assertThat(cpfAutomaton.validateCPF("123.123.123-123")).isFalse();
+  }
+
+  @Test
+  void testInvalidCPFWithTooManyDigits() {
+    assertThat(cpfAutomaton.validateCPF("1213131212121212121")).isFalse();
+  }
+
+  @Test
+  void testInvalidCPFWithTooFewDigitsAndDash() {
+    assertThat(cpfAutomaton.validateCPF("123123123-12")).isFalse();
+  }
+
+  @Test
+  void testEmptyCPF() {
+    assertThat(cpfAutomaton.validateCPF("")).isFalse();
+  }
+
+  @Test
+  void testCPFWithLetters() {
+    assertThat(cpfAutomaton.validateCPF("abc.abc.abc-ab")).isFalse();
+  }
+
+  @Test
+  void testCPFWithSpecialCharacters() {
+    assertThat(cpfAutomaton.validateCPF("123!123@123#12")).isFalse();
+  }
+
+  @Test
+  void testInvalidCPFWithDotAtTheEnd() {
+    assertThat(cpfAutomaton.validateCPF("123.")).isFalse();
+  }
+
+  @Test
+  void testInvalidCPFWithDashAtTheEnd() {
+    assertThat(cpfAutomaton.validateCPF("123-")).isFalse();
+  }
+
+  @Test
+  void testInvalidCPFWithOnlyDotsAndDashes() {
+    assertThat(cpfAutomaton.validateCPF("...-")).isFalse();
+  }
 }
